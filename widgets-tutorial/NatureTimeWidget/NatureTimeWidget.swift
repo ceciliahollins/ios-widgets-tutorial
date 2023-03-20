a00c8cf789835429d7809fb73528ce689d511a0b
Cecilia Hollins <chollins@deloitte.com.au>
2023-03-21 10:55:34 +1100

    provider documentation

--- widgets-tutorial/NatureTimeWidget/NatureTimeWidget.swift
+++ widgets-tutorial/NatureTimeWidget/NatureTimeWidget.swift
@@ -8,22 +8,37 @@
 import WidgetKit
 import SwiftUI
 
+/// The Provider tells WidgetKit when to update the widget's content
+///
+/// There are two options for the provider. Conforming to the IntentTimelineProvider protocol allows for user configurable actions, while the TimelineProvider does not. If your widget does not need user configurations to display, then conform to TimelineProvider instead.
 struct Provider: IntentTimelineProvider {
 
-    typealias Entry = SimpleEntry
+    /// Creates the entry data for the widget to consume at a specific moment
+    typealias Entry = NatureTimeEntry
+    
+    /// The configurable settings of the widget
+    ///
+    /// Define configurations through a custom SiriKit Intent Defintion file. For this project, the configuration is the type of nature setting the user would like to display as the background of the widget.
     typealias Intent = NatureThemeSelectionIntent
     
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), theme: .beach)
+    /// A view to render placeholder UI while waiting for the widget data to load
+    func placeholder(in context: Context) -> NatureTimeEntry {
+        NatureTimeEntry(date: Date(), theme: .beach)
     }
 
-    func getSnapshot(for configuration: NatureThemeSelectionIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), theme: .beach)
+    /// Provides the view for transient situations
+    ///
+    /// An example of a transient situation is previewing the widget in the widget gallery before selecting the widget. It should be quick to load and may show dummy data as to not keep the user waiting.
+    func getSnapshot(for configuration: NatureThemeSelectionIntent, in context: Context, completion: @escaping (NatureTimeEntry) -> ()) {
+        let entry = NatureTimeEntry(date: Date(), theme: .beach)
         completion(entry)
     }
 
+    /// Provides the timeline entries for the current time and state of a widget
+    ///
+    /// This is the main logic for updating the widgets data overtime. If the widget is showing static UI, This function can return just 1 timeline entry. Otherwise, append the entries into an array based on how many and the data that will be passed to return.
     func getTimeline(for configuration: NatureThemeSelectionIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
+        var entries: [NatureTimeEntry] = []
         
         let selectedTheme = theme(for: configuration)
         
@@ -33,13 +48,14 @@ struct Provider: IntentTimelineProvider {
 
         for offset in 0 ..< 60 * 24 {
             let entryDate = Calendar.current.date(byAdding: .minute, value: offset, to: midnight)!
-            entries.append(SimpleEntry(date: entryDate, theme: selectedTheme))
+            entries.append(NatureTimeEntry(date: entryDate, theme: selectedTheme))
         }
 
         let timeline = Timeline(entries: entries, policy: .after(nextMidnight))
         completion(timeline)
     }
     
+    /// A helper function not required by the protocol to convert the configuration intent into the project defined theme
     func theme(for configuration: NatureThemeSelectionIntent) -> NatureTheme {
         switch configuration.theme {
         case .beach:
@@ -54,7 +70,7 @@ struct Provider: IntentTimelineProvider {
     }
 }
 
-struct SimpleEntry: TimelineEntry {
+struct NatureTimeEntry: TimelineEntry {
     let date: Date
     let theme: NatureTheme
 }
@@ -91,17 +107,17 @@ struct NatureTimeWidget: Widget {
 struct NatureTimeWidget_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            NatureTimeWidgetEntryView(entry: SimpleEntry(date: Date(), theme: .beach))
+            NatureTimeWidgetEntryView(entry: NatureTimeEntry(date: Date(), theme: .beach))
                 .previewContext(WidgetPreviewContext(family: .systemSmall))
             
-            NatureTimeWidgetEntryView(entry: SimpleEntry(date: Date(), theme: .beach))
+            NatureTimeWidgetEntryView(entry: NatureTimeEntry(date: Date(), theme: .beach))
                 .previewContext(WidgetPreviewContext(family: .systemSmall))
                 .redacted(reason: .placeholder)
             
-            NatureTimeWidgetEntryView(entry: SimpleEntry(date: Date(), theme: .beach))
+            NatureTimeWidgetEntryView(entry: NatureTimeEntry(date: Date(), theme: .beach))
                 .previewContext(WidgetPreviewContext(family: .systemMedium))
             
-            NatureTimeWidgetEntryView(entry: SimpleEntry(date: Date(), theme: .beach))
+            NatureTimeWidgetEntryView(entry: NatureTimeEntry(date: Date(), theme: .beach))
                 .previewContext(WidgetPreviewContext(family: .systemLarge))
         }
     }
